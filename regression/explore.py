cbab7abc8843c4c0954b20af2d14bbf7e2437150
John Salas <johndsalas@gmail.com>
2019-10-11 10:48:32 -0500

    finished

--- regression/explore.py
+++ regression/explore.py
@@ -32,15 +32,70 @@ dataframe = train
 
 def plot_variable_pairs(dataframe):
 
-    j = sns.scatterplot("tenure", "monthly_charges", data=dataframe,)
+
+    sns.regplot("tenure", "monthly_charges", data=dataframe, color='orange')
+    sns.scatterplot("tenure", "monthly_charges", data=dataframe)
     plt.show()
 
-plot_variable_pairs(dataframe)
-# Write a function, months_to_years(tenure_months, df) that returns your dataframe with a new feature tenure_years, 
+    sns.regplot("tenure", "total_charges", data=dataframe, color='orange')
+    sns.scatterplot("tenure", "total_charges", data=dataframe)
+    plt.show()
+
+    sns.regplot("monthly_charges", "total_charges", data=dataframe, color='orange')
+    sns.scatterplot("monthly_charges", "total_charges", data=dataframe)
+    plt.show()
+
+
+# Write a function, months_to_years(df) that returns your dataframe with a new feature tenure_years, 
 # in complete years as a customer.
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import warnings
+warnings.filterwarnings("ignore")
+
+import env
+import wrangle
+import split_scale
+def months_to_years(df):
+
+    df['tenure_years'] = (df.tenure/12).round().astype('int')
+
+    return df
+
+df = months_to_years(df)
+
+
 
 # Write a function, plot_categorical_and_continous_vars(categorical_var, continuous_var, df), 
 # that outputs 3 different plots for plotting a categorical variable with a continuous variable, 
 # e.g. tenure_years with total_charges. 
 # For ideas on effective ways to visualize categorical with continuous: https://datavizcatalogue.com/. 
 # You can then look into seaborn and matplotlib documentation for ways to create plots.
+
+df = months_to_years(df)
+
+categorical_var = 'tenure_years'
+
+continuous_var = 'total_charges'
+
+def plot_categorical_and_continous_vars(categorical_var, continuous_var, df):
+
+    sns.barplot(x=categorical_var,y=continuous_var,data=df)
+    plt.show()
+
+    sns.boxplot(x=categorical_var,y=continuous_var,data=df)
+    plt.show()
+
+    sns.violinplot(x=categorical_var,y=continuous_var,data=df)
+    plt.show()
+
+plot_categorical_and_continous_vars(categorical_var,continuous_var,df)
