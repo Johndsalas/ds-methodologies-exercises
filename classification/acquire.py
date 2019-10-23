6857bc8a6607e727a765c49a76d63698db0c7631
John Salas <johndsalas@gmail.com>
2019-10-23 09:43:32 -0500

    commit

--- classification/acquire.py
+++ classification/acquire.py
@@ -1,6 +1,17 @@
+import pandas as pd
+import numpy as np
+import acquire as a
+# import warnings
+# warnings.filterwarnings('ignore')
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 import warnings
 warnings.filterwarnings('ignore')
-
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
@@ -107,3 +118,31 @@ def prep_iris(df):
     array = encoder.transform(df.species_name)
     names = list(encoder.inverse_transform(array))
     return names
+
+def prep_titanic(tdf):
+
+    imp_mode = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    imp_mode.fit(tdf[['embarked']])
+    tdf['embarked'] = imp_mode.transform(tdf[['embarked']])
+
+    imp_mode = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    imp_mode.fit(tdf[['embark_town']])
+    tdf['embark_town'] = imp_mode.transform(tdf[['embark_town']])
+
+    imp_mode = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    imp_mode.fit(tdf[['embarked']])
+    tdf['embarked'] = imp_mode.transform(tdf[['embarked']])
+
+    int_encoder = LabelEncoder()
+    int_encoder.fit(tdf.embarked)
+    tdf_embarked = int_encoder.transform(tdf.embarked)
+
+    scaler = MinMaxScaler()
+    scaler.fit(tdf[['fare']])
+    tdf.fare = scaler.transform(tdf[['fare']])
+
+    scaler = MinMaxScaler()
+    scaler.fit(tdf[['age']])
+    tdf.age = scaler.transform(tdf[['age']])
+
+    return tdf
